75730c94bb7af81c72cc98aa571b6a5d074a54d6
EthanCo <fenghenyu@163.com>
2016-08-26 09:17:07 +0800

    espresso test

---
+++ MyEspressoTest/app/src/main/java/com/ethanco/myespressotest/MainActivity.java
@@ -0,0 +1,23 @@
+package com.ethanco.myespressotest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView greetingView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        greetingView = (TextView) findViewById(R.id.greeting);
+    }
+
+    public void greet(View v) {
+        greetingView.setText(R.string.hello);
+    }
+}
